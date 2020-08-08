fe9ff77c25005a016d8552c8d611cf5d4cc5c767
Miten Gajjar <miten3377@gmail.com>
2020-08-08 16:05:10 +0530

    added getIt locator

---
+++ lib/constant.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+// Colors
+const kBackgroundColor = Color(0xFFFEFEFE);
+const kTitleTextColor = Color(0xFF303030);
+// const kTitleTextColor = Colors.white;
+const kBodyTextColor = Color(0xFF4B4B4B);
+const kTextLightColor = Color(0xFF959595);
+// const kTextLightColor = Colors.white;
+const kInfectedColor = Color(0xFFFF8748);
+const kDeathColor = Color(0xFFFF4848);
+const kRecovercolor = Color(0xFF36C12C);
+const kPrimaryColor = Color(0xFF3382CC);
+final kShadowColor = Color(0xFFB7B7B7).withOpacity(.16);
+final kActiveShadowColor = Color(0xFF4056C6).withOpacity(.15);
+
+// Text Style
+const kHeadingTextStyle = TextStyle(
+  fontSize: 22,
+  fontWeight: FontWeight.w600,
+);
+
+const kSubTextStyle = TextStyle(fontSize: 16, color: kTextLightColor,fontFamily: 'fedroka');
+
+const kTitleTextstyle = TextStyle(
+  fontSize: 23,
+  color: kTitleTextColor,
+  fontWeight: FontWeight.normal,
+  fontFamily: 'fedroka'
+);
